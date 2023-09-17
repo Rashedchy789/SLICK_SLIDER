3ecc4c7383d728ab5833b3108deaf0012ff54cb9
RashedChy <136729495+Rashedchy789@users.noreply.github.com>
2023-09-17 03:57:44 -0700

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+$(function () {
+  // SLICK SLIDER STARTS
+  $('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    autoplaySpeed: 1000,
+    autoplay: true,
+    infinite: true,
+    prevArrow: '.left_arrow',
+    nextArrow: '.right_arrow',
+  });
+});
